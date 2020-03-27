1dd16ce2476a95a328c2c9c475913e8bd447f747
monster71 <35062179+monster71@users.noreply.github.com>
2020-03-27 18:05:40 +0800

    httpclient

---
+++ cast/Study/HttpClientStudy/Program.cs
@@ -0,0 +1,18 @@
+﻿using HttpClientStudy.Demo;
+using System;
+
+namespace HttpClientStudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            HttpClientDemo.Second();
+
+            Console.ReadKey(true);
+
+        }
+
+    }
+}
