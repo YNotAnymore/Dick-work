62eb73401a85dae543f39dc3e0d9ce6215e4918a
monster71 <35062179+monster71@users.noreply.github.com>
2020-04-16 17:29:48 +0800

    async

---
+++ cast/Sample/AnyThing/Program.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Text.Json.Serialization;
+
+namespace AnyThing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var data = "{\"data\":1}";
+
+            var empty = JsonConvert.DeserializeObject<string>(data);
+
+            Console.WriteLine(empty);
+
+            var str = System.Text.Json.JsonSerializer.Deserialize<string>(data);
+
+            Console.WriteLine(data);
+
+            Console.WriteLine("Hello World!");
+
+            Console.ReadKey(true);
+
+        }
+    }
+}
